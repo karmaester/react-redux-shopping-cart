946f4c2276069d612e94bb4d7b30367b3271ebc8
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-12 00:27:32 -0400

    Add fade in to products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
-import formatCurrency from '../util'
+import React, { Component } from 'react';
+import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
 
 export default class Products extends Component {
     render() {
         return (
             <div>
-                <ul className="products">
-                    {this.props.products.map(product => (
-                        <li key={product._id} >
-                            <div className="product">
-                                <a href={"#" + product._id}>
-                                    <img src={product.image} alt="product" />
-                                    <p>
-                                        {product.title}
-                                    </p>
-                                </a>
-                                <div className="product-price">
-                                    <div className="">
-                                        {formatCurrency(product.price)}
+                <Fade bottom cascade>
+                    <ul className="products">
+                        {this.props.products.map(product => (
+                            <li key={product._id} >
+                                <div className="product">
+                                    <a href={"#" + product._id}>
+                                        <img src={product.image} alt="product" />
+                                        <p>
+                                            {product.title}
+                                        </p>
+                                    </a>
+                                    <div className="product-price">
+                                        <div className="">
+                                            {formatCurrency(product.price)}
+                                        </div>
+                                        <button onClick={() => this.props.addToCart(product)} className="button primary">
+                                            Add to cart
+                                        </button>
                                     </div>
-                                    <button onClick={() => this.props.addToCart(product)} className="button primary">
-                                        Add to cart
-                                    </button>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </Fade>
             </div>
         )
     }
