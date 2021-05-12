95d370b988226255b9429cf67b171f53d570e86f
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-12 13:21:05 -0400

    Add cart animations

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
 
 export default class Cart extends Component {
     constructor(props) {
@@ -39,6 +40,7 @@ export default class Cart extends Component {
             </div>
             <div className="">
                 <div className="cart">
+                    <Fade left cascade>
                     <ul className="cart-items">
                         {cartItems.map(item => (
                             <li key={item._id}>
@@ -57,6 +59,7 @@ export default class Cart extends Component {
                             </li>
                         ))}
                     </ul>
+                    </Fade>
                 </div>
                 {cartItems.length !== 0 && (
                     <div className="">
