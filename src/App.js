f2264f488aaeb36c9e8e1d41d113d41ad27f9e5b
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-11 23:21:58 -0400

    Add to local storage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")): [],
       size: "",
       sort: "",
     }
@@ -20,6 +20,8 @@ class App extends React.Component {
     this.setState({
       cartItems: cartItems.filter(x=>x._id !== product._id),
     });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x=>x._id !== product._id)));
+
   };
 
   addToCart = (product) => {
@@ -35,6 +37,7 @@ class App extends React.Component {
       cartItems.push({...product, count: 1})
     }
     this.setState({cartItems: cartItems})
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
   sortProducts = (event) => {
